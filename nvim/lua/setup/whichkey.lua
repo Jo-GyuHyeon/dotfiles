9659bd2eb366864a6acd908c75a10a3c0fcb1279
hahunwoo <devin.h.code@gmail.com>
2021-12-26 14:57:57 +0900

    Add gitsigns fun to whichkey

--- nvim/lua/setup/whichkey.lua
+++ nvim/lua/setup/whichkey.lua
@@ -1,11 +1,11 @@
 local wk = require("which-key")
-wk.setup {
+wk.setup { 
   plugins = {
-    marks = false,
-    registers = false,
-    spelling = {enabled = false, suggestions = 20},
-    presets = {operators = false, motions = false, text_objects = false, windows = false, nav = false, z = false, g = false}
-  }
+  marks = false,
+  registers = false,
+  spelling = {enabled = false, suggestions = 20},
+  presets = {operators = false, motions = false, text_objects = false, windows = false, nav = false, z = false, g = false}
+}
 }
 local Terminal = require('toggleterm.terminal').Terminal
 local toggle_float = function()
@@ -38,13 +38,14 @@ local mappings = {
   g = {
     name = "Git",
     g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
-    j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-    k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+    n = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+    p = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
     l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-    p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+    k = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
     r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-    R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+    R = { "<cmd>lua require 'gitsigns'.reset_buffer_index()<cr>", "Reset Buffer" },
     s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+    S = { "<cmd>lua require 'gitsigns'.stage_buffer()<cr>", "Stage Buffer" },
     u = {
       "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
       "Undo Stage Hunk",
